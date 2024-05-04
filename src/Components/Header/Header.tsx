2c24d98c0874fe2c3dea964febd1066ce6162e9e
Ghayth Sabeaallil <gs222mh@gmail.com>
2024-05-04 14:15:17 +0200

    fix the fetch of the search bar which I change it to to the search box

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import "../../Styles/Components/Header.scss"
-import useFetchSearchBar from "../../Hooks/useFetch/useFetchSearchBar";
+import "../../Styles/Components/Header.scss";
 import SearchBox from "../SearchBox/SearchBox";
 import SelectDropDown from "../SelectDropDown/SelectDropDown";
 
@@ -9,47 +8,62 @@ function Header() {
     const [select, setSelect] = useState<string>("title");
     const [search, setSearch] = useState<string>("");
 
-    const [data] = useFetchSearchBar({ select, search });
-    const handleSelectChange: React.ChangeEventHandler<HTMLSelectElement> = (event) => {
+    const handleSelectChange: React.ChangeEventHandler<HTMLSelectElement> = (
+        event
+    ) => {
         setSelect(event.target.value);
     };
-    const handleSearchChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    const handleSearchChange: React.ChangeEventHandler<HTMLInputElement> = (
+        event
+    ) => {
         setSearch(event.target.value);
+        let search_box = document.getElementById(
+            "search-box-container"
+        ) as HTMLElement;
+        if (event.target.value != "") {
+            if (search_box) {
+                search_box.style.display = "flex";
+            }
+        } else {
+            if (search_box) {
+                search_box.style.display = "none";
+            }
+        }
     };
-    if (search != "") {
-        let search_box = document.getElementById("search-box-container") as HTMLElement;
-        if (search_box) {
-            search_box.style.display = "flex";
-        }
-    }
-    else {
-        let search_box = document.getElementById("search-box-container") as HTMLElement;
-        if (search_box) {
-            search_box.style.display = "none";
-        }
-    }
-
 
     return (
-        <><header className="navbar-dekstop">
-            <a className="logo" href="/"><img src="/logo.png" alt="logo" /></a>
-            <div className="search-bar">
-                <div className="input-bar">
-
-
-                    <SelectDropDown handleSelectChange={handleSelectChange} items={selectList} value={select} />
-                    <input type="text" placeholder="title/author/isbn" onChange={handleSearchChange} /></div>
-
-                <div id="search-box-container" className="search-box-container">
-                    {(data?.numFound! > 0 && search != "") ? data?.docs.slice(0, 20).map((book) => (<SearchBox title={book.title} cover_i={book.cover_i} author={book.author_name} />)) : null}
-                    <a className="show-all" href={`/search?${select}=${search}`}>Show all Results</a>
+        <>
+            <header className="navbar-dekstop">
+                <a className="logo" href="/">
+                    <img src="/logo.png" alt="logo" />
+                </a>
+                <div className="search-bar">
+                    <div className="input-bar">
+                        <SelectDropDown
+                            handleSelectChange={handleSelectChange}
+                            items={selectList}
+                            value={select}
+                        />
+                        <input
+                            type="text"
+                            placeholder="title/author/isbn"
+                            onChange={handleSearchChange}
+                        />
+                    </div>
+                    <div id="search-box-container" className="search-box-container">
+                        {search != "" ? (
+                            <SearchBox search={search} select={select} />
+                        ) : null}
+                        <a className="show-all" href={`/search?${select}=${search}`}>
+                            Show all Results
+                        </a>
+                    </div>
                 </div>
-            </div>
-            <select>
-                <option value="title">Favourit Books</option>
-                <option value="author">Read Books</option>
-            </select>
-        </header>
+                <select>
+                    <option value="title">Favourit Books</option>
+                    <option value="author">Read Books</option>
+                </select>
+            </header>
         </>
     );
 }
--- src/Components/SearchBox/SearchBox.tsx
+++ src/Components/SearchBox/SearchBox.tsx
@@ -1,23 +1,36 @@
-import "../../Styles/Components/SearchBox.scss"
+import useFetchSearchBar from "../../Hooks/useFetch/useFetchSearchBar";
+import "../../Styles/Components/SearchBox.scss";
 
 type searchBoxProps = {
-    title: string,
-    cover_i: number,
-    author: string[]
-}
+    search: string;
+    select: string;
+};
 
+const SearchBox = ({ select, search }: searchBoxProps) => {
+    const [data] = useFetchSearchBar({ select, search });
 
-const SearchBox = ({ cover_i, title, author }: searchBoxProps) => {
     return (
         <>
-            <div className="search-item">
-                {cover_i == null ? <div className="bok-img-div"></div> : <img className="bok-img" src={`https://covers.openlibrary.org/b/id/${cover_i}-S.jpg`} alt={title} />}
-                <div className="info">
-                    <p className="title">{title}</p>
-                    <p className="author">By: {author}</p>
-                </div>
-            </div>
+            {data?.numFound! > 0
+                ? data?.docs.slice(0, 20).map((book) => (
+                    <div className="search-item">
+                        {book.cover_i == null ? (
+                            <div className="bok-img-div"></div>
+                        ) : (
+                            <img
+                                className="bok-img"
+                                src={`https://covers.openlibrary.org/b/id/${book.cover_i}-S.jpg`}
+                                alt={book.title}
+                            />
+                        )}
+                        <div className="info">
+                            <p className="title">{book.title}</p>
+                            <p className="author">By: {book.author_name}</p>
+                        </div>
+                    </div>
+                ))
+                : null}
         </>
-    )
-}
+    );
+};
 export default SearchBox;
